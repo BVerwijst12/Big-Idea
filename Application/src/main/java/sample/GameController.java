28e97cb9977efadae626687f75488ccbc1bdbfc1
bram verwijst <bram.verwijst12@gmail.com>
2019-06-05 13:46:33 +0200

    its all gone

--- Application/src/main/java/sample/GameController.java
+++ Application/src/main/java/sample/GameController.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,10 +15,16 @@ public class GameController implements Initializable {
     public TextField TbName;
     public TextField TbPassword;
 
+    public GameController(){
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+
 
     public void Login(ActionEvent actionEvent) {
         System.out.println("User" + TbName.getText() + "logged in");
